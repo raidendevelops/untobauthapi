33e19dd0389c26a72674b653663445bf718ba38c
raidendevelops <raidensch0@gmail.com>
2025-02-25 18:07:44 -0500

    Update firebaseConfig.js

--- functions/firebaseConfig.js
+++ functions/firebaseConfig.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins for simplicity
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-app.get('/firebase-config', (req, res) => {
-    res.json({
-        apiKey: "AIzaSyCOOOzdBKLfQpToFW6bt5XW-tgbOsb7-_s",
-        authDomain: "untobauth.firebaseapp.com",
-        projectId: "untobauth",
-        storageBucket: "untobauth.firebasestorage.app",
-        messagingSenderId: "595245764662",
-        appId: "1:595245764662:web:ccd0b84b22745ca240c897",
-        measurementId: "G-LWNEY850D4"
-    });
-});
-
-module.exports.handler = serverless(app);
+// functions/firebaseConfig.js
+exports.handler = async function(event, context) {
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*", // Allow all origins for simplicity
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        },
+        body: JSON.stringify({
+            apiKey: "AIzaSyCOOOzdBKLfQpToFW6bt5XW-tgbOsb7-_s",
+            authDomain: "untobauth.firebaseapp.com",
+            projectId: "untobauth",
+            storageBucket: "untobauth.firebasestorage.app",
+            messagingSenderId: "595245764662",
+            appId: "1:595245764662:web:ccd0b84b22745ca240c897",
+            measurementId: "G-LWNEY850D4"
+        })
+    };
+};
