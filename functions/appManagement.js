dcad5723071a38418fd8a35735ea137b4bdb82c2
raidendevelops <raidensch0@gmail.com>
2025-02-26 18:00:03 -0500

    Update appManagement.js

--- functions/appManagement.js
+++ functions/appManagement.js
@@ -1,4 +1,74 @@
+const { google } = require('googleapis');
+const drive = google.drive('v3');
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+const TOKEN_PATH = path.join(__dirname, 'token.json');
+const CREDENTIALS_PATH = path.join(__dirname, 'client_secret.json');
+
 let apps = [];
+
+const authorize = (credentials, callback) => {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getAccessToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+};
+
+const getAccessToken = (oAuth2Client, callback) => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+};
+
+const uploadFile = (auth, fileName, filePath) => {
+  const fileMetadata = {
+    name: fileName,
+  };
+  const media = {
+    mimeType: 'application/json',
+    body: fs.createReadStream(filePath),
+  };
+  drive.files.create(
+    {
+      auth,
+      resource: fileMetadata,
+      media,
+      fields: 'id',
+    },
+    (err, file) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('File Id:', file.id);
+      }
+    }
+  );
+};
 
 exports.handler = async (event) => {
   try {
@@ -31,8 +101,13 @@ const handlePostRequest = async (event) => {
   };
 
   apps.push(newApp);
-  console.log('New app created:', newApp);
-  console.log('Data after adding new app:', apps);
+
+  const filePath = path.join(__dirname, 'apps.json');
+  fs.writeFileSync(filePath, JSON.stringify(apps, null, 2));
+
+  authorize(JSON.parse(fs.readFileSync(CREDENTIALS_PATH)), (auth) => {
+    uploadFile(auth, 'apps.json', filePath);
+  });
 
   return { statusCode: 201, body: JSON.stringify(newApp) };
 };
